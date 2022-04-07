78889c93feb0708629b4cd53bc6cff879521302a
TheKafer <38733280+TheKafer@users.noreply.github.com>
2022-04-06 21:20:51 -0500

    Add messages to the Env_Variables

--- src/errors/env-variables-not-defined.ts
+++ src/errors/env-variables-not-defined.ts
@@ -2,17 +2,14 @@ import { BaseError } from "./base-error";
 
 export class EnvVariablesNotDefinedError extends BaseError {
     statusCode = 503;
-    reason = 'Error getting the env variables'
 
-    constructor() {
+    constructor(public message: string) {
         super('Error getting the env variables');
         // Only because we are extending a built in class
         Object.setPrototypeOf(this, EnvVariablesNotDefinedError.prototype);
     }
 
     serializeErros() {
-        return [
-            {message: this.reason}
-        ];
+        return [{message: this.message}]
     }
 }
