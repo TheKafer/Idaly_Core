664854dafe0ae8105df599063940a00144e43c51
TheKafer <38733280+TheKafer@users.noreply.github.com>
2022-05-21 12:28:34 -0500

    Tracker of errors

--- build/middlewares/error-handler.js
+++ build/middlewares/error-handler.js
@@ -6,6 +6,10 @@ const errorHandler = (err, req, res, next) => {
     if (err instanceof base_error_1.BaseError) {
         return res.status(err.statusCode).send({ errors: err.serializeErros() });
     }
+    else {
+        console.log('Uncontrolled Error:');
+        console.log(err);
+    }
     res.status(400).send({
         errors: [{ message: 'Something went wrong' }]
     });
