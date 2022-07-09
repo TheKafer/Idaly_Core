eddce88170bcbf099630ef666e84c2b7083a6ea6
TheKafer <38733280+TheKafer@users.noreply.github.com>
2022-07-09 11:52:21 -0500

    Delete method "get nodes"

--- build/services/schema.d.ts
+++ build/services/schema.d.ts
@@ -1,11 +1,9 @@
 import { SchemaErrorInterface } from "../interfaces/schema-error";
-import { Node } from "../interfaces/node";
 export declare class SchemaManager {
     static compare(schema: any, suppliedJson: any, errors?: SchemaErrorInterface[]): SchemaErrorInterface[];
     static compareArray(key: string, array: any[], schemaArray: any[], errors: SchemaErrorInterface[]): SchemaErrorInterface[];
     static validateSchema(schema: any, errors?: SchemaErrorInterface[]): SchemaErrorInterface[];
     static validateArrayOfSchema(array: any[], key: string, errors: SchemaErrorInterface[]): SchemaErrorInterface[];
-    static getNodes(schema: any, nodes?: Node[], level?: number, parentName?: string): Node[];
     static getFieldOfSchema(obj: any): string;
     static getField(obj: any): string;
     static isAllowedField(obj: string): boolean;
