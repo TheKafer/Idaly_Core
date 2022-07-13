a82b0141f1ba2a72249c6e63d83f96222ff67006
TheKafer <38733280+TheKafer@users.noreply.github.com>
2022-07-13 16:20:58 -0500

    FIx operators

--- build/interfaces/operator.d.ts
+++ build/interfaces/operator.d.ts
@@ -1,8 +1,9 @@
 import { OperatorTypes } from "../enums/operator-types";
+import { Types } from "../enums/types";
 import { Argument } from "./argument";
 export interface Operator {
     name: string;
     type: OperatorTypes;
-    arguments: Argument[];
-    return: OperatorTypes;
+    arguments?: Argument[];
+    return: Types;
 }
--- src/interfaces/operator.ts
+++ src/interfaces/operator.ts
@@ -1,9 +1,10 @@
 import { OperatorTypes } from "../enums/operator-types";
+import { Types } from "../enums/types";
 import { Argument } from "./argument";
 
 export interface Operator {
     name: string;
     type: OperatorTypes;
-    arguments: Argument[];
-    return: OperatorTypes;
+    arguments?: Argument[];
+    return: Types;
 }
